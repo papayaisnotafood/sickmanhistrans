658c0d877d99329d54238844ffb6c3f464dc4160
papayaisnotafood <70697738+papayaisnotafood@users.noreply.github.com>
2021-05-30 20:11:17 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long time_now = time(0);
+    long long dis= (time_now - 1562256000)/(12 * 60 * 60) + 1;
+    cout << dis;
+}
