2333f5cd8feac41242e50e3c5d1dab3656f00a06
papayaisnotafood <70697738+papayaisnotafood@users.noreply.github.com>
2021-05-30 20:18:04 +0800

    效能優化

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    cout.sync_with_stdio(false);
     long long time_now = time(0);
     long long dis= (time_now - 1562256000)/(12 * 60 * 60) + 1;
     cout << dis;
